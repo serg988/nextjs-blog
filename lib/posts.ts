71ac85d2fe6535a67f65f87b8e4dd363d3cc6c4c
serg988 <42653178+serg988@users.noreply.github.com>
2022-12-05 18:04:58 +0300

    converted to TS

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,75 +1,53 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-
 import { remark } from 'remark'
 import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
-console.log("🚀 ~ file: posts.js:6 ~ postsDirectory", postsDirectory)
 
 export function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
-  console.log("🚀 ~ file: posts.js:10 ~ getSortedPostsData ~ fileNames", fileNames)
-  const allPostsData = fileNames.map((fileName) => {
+  const allPostsData = fileNames.map(fileName => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '')
-    console.log("🚀 ~ file: posts.js:15 ~ allPostsData ~ id", id)
 
     // Read markdown file as string
     const fullPath = path.join(postsDirectory, fileName)
-    console.log("🚀 ~ file: posts.js:19 ~ allPostsData ~ fullPath", fullPath)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
 
     // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents)
-    console.log("🚀 ~ file: posts.js:24 ~ allPostsData ~ matterResult", matterResult.data)
 
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data,
+      ...(matterResult.data as { date: string; title: string })
     }
   })
   // Sort posts by date
-  return allPostsData.sort(({ date: a }, { date: b }) => {
-    if (a < b) {
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
       return 1
-    } else if (a > b) {
-      return -1
     } else {
-      return 0
+      return -1
     }
   })
 }
 
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory)
-
-  // Returns an array that looks like this:
-  // [
-  //   {
-  //     params: {
-  //       id: 'ssg-ssr'
-  //     }
-  //   },
-  //   {
-  //     params: {
-  //       id: 'pre-rendering'
-  //     }
-  //   }
-  // ]
-  return fileNames.map((fileName) => {
+  return fileNames.map(fileName => {
     return {
       params: {
-        id: fileName.replace(/\.md$/, ''),
-      },
+        id: fileName.replace(/\.md$/, '')
+      }
     }
   })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
@@ -81,12 +59,11 @@ export async function getPostData(id) {
     .use(html)
     .process(matterResult.content)
   const contentHtml = processedContent.toString()
-  console.log("🚀 ~ file: posts.js:84 ~ getPostData ~ contentHtml", contentHtml)
 
   // Combine the data with the id and contentHtml
   return {
     id,
     contentHtml,
-    ...matterResult.data,
+    ...(matterResult.data as { date: string; title: string })
   }
 }
